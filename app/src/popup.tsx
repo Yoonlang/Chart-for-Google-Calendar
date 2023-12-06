1e3c18cd6cb2b076eae9689d76d3c0ebf9081c14
yoonlang <cdt9473@gmail.com>
2023-12-06 23:16:29 +0900

    Implement simple chart for time spending

--- app/src/popup.tsx
+++ app/src/popup.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import {
   Chart as ChartJS,
@@ -8,43 +8,98 @@ import {
   LinearScale,
 } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import { getMinuites, getThisMondayMidnight, getTodayMidnight } from "./util";
 
 ChartJS.register(ArcElement, Tooltip, Legend, LinearScale);
 
-const data = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
-    },
-  ],
+const URLS = {
+  CALENDAR_LIST: "https://www.googleapis.com/calendar/v3/users/me/calendarList",
+  EVENTS_PRE: "https://www.googleapis.com/calendar/v3/calendars/",
+  EVENTS_SUF: "/events",
 };
 
 const Popup = () => {
+  const [calendarData, setCalendarData] = useState<any>();
+  const [chartData, setChartData] = useState<any>();
+
+  useEffect(() => {
+    chrome.identity.getAuthToken({ interactive: true }, async (token) => {
+      const headers = new Headers({
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      });
+
+      const calendarListResponse = await fetch(URLS.CALENDAR_LIST, {
+        headers,
+      });
+      const calendarListData = await calendarListResponse.json();
+      const calendarList = calendarListData.items;
+
+      const thisWeekResponse = await Promise.all(
+        calendarList.map((c) =>
+          fetch(
+            `${URLS.EVENTS_PRE}${c.id}${
+              URLS.EVENTS_SUF
+            }/?timeMin=${getThisMondayMidnight().toISOString()}&timeMax=${getTodayMidnight().toISOString()}&maxResults=1000`,
+            {
+              headers,
+            }
+          )
+        )
+      );
+      const thisWeekData = await Promise.all(
+        thisWeekResponse.map(async (res) => await res.json())
+      );
+
+      setCalendarData(
+        calendarList.map(({ id, backgroundColor, summary }, idx) => {
+          return {
+            id,
+            backgroundColor,
+            summary,
+            events: thisWeekData[idx].items.map(
+              ({ start, end, colorId, id }) => {
+                return { start, end, colorId, id };
+              }
+            ),
+          };
+        })
+      );
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!calendarData) {
+      return;
+    }
+
+    setChartData({
+      labels: calendarData.map((d) => d.summary),
+      datasets: [
+        {
+          label: "Time spent(m)",
+          data: calendarData.map(({ events }) => {
+            return getMinuites(
+              events
+                .filter((e) => e.start.dateTime)
+                .map((e) => {
+                  return (
+                    new Date(e.end.dateTime).getTime() -
+                    new Date(e.start.dateTime).getTime()
+                  );
+                })
+                .reduce((acc, cur) => acc + cur, 0)
+            );
+          }),
+          backgroundColor: calendarData.map((d) => d.backgroundColor),
+        },
+      ],
+    });
+  }, [calendarData]);
+
   return (
     <>
-      <div>
-        popup
-        <Doughnut data={data} />
-      </div>
+      <div>{chartData && <Doughnut data={chartData} />}</div>
     </>
   );
 };
---
+++ app/src/util.ts
@@ -0,0 +1,33 @@
+export const getThisMondayMidnight = () => {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+
+  const daysUntilMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+  const monday = new Date(today);
+  monday.setDate(today.getDate() - daysUntilMonday);
+  monday.setHours(0, 0, 0, 0);
+  return monday;
+};
+
+export const getTodayMidnight = () => {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  return today;
+};
+
+export const getHHMM = (ms) => {
+  const seconds = Math.floor(ms / 1000);
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+
+  const formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
+  return formattedTime;
+};
+
+export const getMinuites = (ms) => {
+  const seconds = Math.floor(ms / 1000);
+  const minutes = Math.floor(seconds / 60);
+  return minutes;
+};
