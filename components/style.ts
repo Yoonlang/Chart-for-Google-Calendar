690e08d8049984efb0386e2154325ac5ece372b2
yoonlang <cdt9473@gmail.com>
2025-03-27 04:47:06 +0900

    style: width, height 배경 처리

--- components/style.ts
+++ components/style.ts
@@ -11,6 +11,11 @@ html, body {
     min-height: 400px;
 }
 
+#app-container {
+    width: 100%;
+    height: 100%;
+}
+
 .rs-picker-menu {
     top: calc(50% - 183px + 20px) !important;
     left: calc(50% - 264px) !important;
